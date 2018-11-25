6be2926e07dce4fe8d506808d7dcc884a147b5f9
Adam Dudley <dudleyadam@gmail.com>
2018-11-25 17:14:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,9 +7,9 @@
 def prime?(integer)
   return false if integer < 2 # retunrs false b/c any number less than 2 cannot be a prime number
   return true if integer == 3 || integer == 2 # returns true b/c 3 and 2 are the lowest numbers that can be prime
-    if (2...integer-1).any?{|i| integer % i == 0}
+    if (2...integer-1).any?{|i| integer % i == 0} # says that if any number between 2 and the integer less one returns a value other than false or nil it returns true, the new variable is i, if the integer divided by i is equalivalent to 0 then also return false
       false
-    else
+    else # if any value returned in the if statement doesn't return false, return true
       true
     end
 end
